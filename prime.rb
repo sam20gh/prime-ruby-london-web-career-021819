8cf67d7abf379dfb34e1b3e7bc642a7bd520a6fc
sam ghoreishi <sam20gh@gmail.com>
2019-02-06 22:27:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,9 @@
+require 'benchmark'
+require 'bigdecimal/math'
+
+# calculate pi to 10k digits
+puts Benchmark.measure
+
 def prime?(integer)
     return false if integer < 2
     (2..integer - 1).each do |x|
